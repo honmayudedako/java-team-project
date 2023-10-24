5fdc364b3974c87f9055e7b2e0a4943460014ffd
本間瑠 <yudedako0213@gmail.com>
2023-10-24 18:41:56 +0900

    add insertDateTime and updateDateTime

--- src/main/java/model/entity/CustomerBean.java
+++ src/main/java/model/entity/CustomerBean.java
@@ -1,70 +1,88 @@
 package model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class CustomerBean {
 	
-	@Getter @Setter private String name;
-	@Getter @Setter private String nameKana;
-	@Getter @Setter private String postCode;
-	@Getter @Setter private String areaCode;
-	@Getter @Setter private String gender;
-	@Getter @Setter private String birthday;
-	@Getter @Setter private String phoneNumber;
+	private int id;
+	private String name;
+	private String nameKana;
+	private String postCode;
+	private String areaCode;
+	private String gender;
+	private String birthday;
+	private String phoneNumber;
+	private String insertDateTime;
+	private String updateDateTime;
 			
 	public CustomerBean(){}
 	
-//	public String getName() {
-//		return this.name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//	public String getNameKana() {
-//		return this.nameKana;
-//	}
-//	public void setNameKana(String nameKana) {
-//		this.nameKana = nameKana;
-//	}
-//	
-//	public String getPostCode() {
-//		return this.postCode;
-//	}
-//	public void setPostCode(String postCode) {
-//		this.postCode = postCode;
-//	}
-//	
-//	public String getAreaCode() {
-//		return this.areaCode;
-//	}
-//	public void setAreaCode(String areaCode) {
-//		this.areaCode = areaCode;
-//	}
-//	
-//	public String getGender() {
-//		return this.gender;
-//	}
-//	public void setGender(String gender) {
-//		this.gender = gender;
-//	}
-//	
-//	public String getBirthday() {
-//		return this.birthday;
-//	}
-//	public void setBirthday(String birthday) {
-//		this.birthday = birthday;
-//	}
-//	
-//	public String getPhoneNumber() {
-//		return this.phoneNumber;
-//	}
-//	public void setPhoneNumber(String phoneNumber) {
-//		this.phoneNumber = phoneNumber;
-//	}
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getNameKana() {
+		return this.nameKana;
+	}
+	public void setNameKana(String nameKana) {
+		this.nameKana = nameKana;
+	}
+	
+	public String getPostCode() {
+		return this.postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	
+	public String getAreaCode() {
+		return this.areaCode;
+	}
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+	
+	public String getGender() {
+		return this.gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getBirthday() {
+		return this.birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
+	public String getInsertDateTime() {
+		return this.insertDateTime;
+	}
+	public void setInsertDateTime(String InsertDateTime) {
+		this.insertDateTime = InsertDateTime;
+	}
+
+	public String getUpdateDateTime() {
+		return this.updateDateTime;
+	}
+
+	public void setUpdateDateTime(String UpdatedateTime) {
+		this.updateDateTime = UpdatedateTime;
+	}
 }
-
-
-
